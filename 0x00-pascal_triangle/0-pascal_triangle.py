9fb13f1db81e1283fb22521a32f98eee70d843c1
joseph0017 <josephogiku96@gmail.com>
2023-06-28 04:38:23 -0700

    pascal triangle3

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,6 +2,8 @@
 """module for pascal triangle"""
 def pascal_triangle(n):
    """pascal triangle"""
+   if n == 0:
+      return []
    triangle = [[1]*(i+1) for i in range(n)]
    for i in range(n):
       for j in range(1,i):
