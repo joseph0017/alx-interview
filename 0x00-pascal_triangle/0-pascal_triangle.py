427bfb90ead2f205efd3547fe09ae86f98e37b9c
joseph0017 <josephogiku96@gmail.com>
2023-06-29 00:46:11 -0700

    fixing2

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,11 +3,11 @@
 
 
 def pascal_triangle(n):
-   """pascal triangle function with n as an argument"""
-   if n <= 0:
-      return []
-   triangle = [[1]*(i+1) for i in range(n)]
-   for i in range(n):
-      for j in range(1,i):
-         triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
-   return triangle
+    """pascal triangle function that takes n as an argument"""
+    if n <= 0:
+        return []
+    triangle = [[1]*(i+1) for i in range(n)]
+    for i in range(n):
+        for j in range(1, i):
+            triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+    return triangle     
