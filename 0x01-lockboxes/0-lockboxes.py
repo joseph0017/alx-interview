bd6be50f03cfe2f523cde6bfe879dc1d5e8b3898
joseph0017 <josephogiku96@gmail.com>
2023-07-04 12:07:55 -0700

    unlock boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""function that unlocks all boxes if keys match"""
+
+
+def canUnlockAll(boxes):
+	"""function to unlock boxes"""
+	open_box = [0]
+	for i in open_box:
+		for j in boxes[i]:
+			if j not in open_box:
+				open_box.append(j)
+	return len(open_box) == len(boxes)
