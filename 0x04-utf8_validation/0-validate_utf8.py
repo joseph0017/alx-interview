f04c1f4786d5e5e0300449ac50d9185d07d2ba18
joseph0017 <josephogiku96@gmail.com>
2023-07-29 14:37:17 +0100

    fixes

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,7 +7,13 @@ def validUTF8(data):
     count = 0
     for num in data:
         if count == 0:
-            if (num >> 7) != 0:
+            if (num >> 5) == 0b110:
+                count = 1
+            elif (num >> 4) == 0b1110:
+                count = 2
+            elif (num >> 3) == 0b11110:
+                count = 3
+            elif (num >> 7) != 0:
                 return False
         else:
             if (num >> 6) != 0b10:
